67bba05be4dd21c9bdc56dc4961a725930184a98
MIFDAL <saidilmifdal15@gmail.com>
2026-06-04 21:02:04 +0800

    Deploy NutriVision AI Model API

--- model (AI)/models/testmodel.py
+++
@@ -1,9 +0,0 @@
-# test_model.py
-
-import tensorflow as tf
-
-model = tf.saved_model.load(
-    "C:\\Users\\Hype AMD\\Nutrivisonweb\\NutriVision-Web\\model (AI)\\models\\food_saved_model"
-)
-
-print("Model Loaded Successfully")
